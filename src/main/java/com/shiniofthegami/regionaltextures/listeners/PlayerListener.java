afbddb4f6cbc7e0409f1c2b15c8f8b9e817a6e35
Shiggysama <ctbuschmann@gmail.com>
2015-03-24 03:09:20 +0100

    changing listener from login to joinevent

--- src/main/java/com/shiniofthegami/regionaltextures/listeners/PlayerListener.java
+++ src/main/java/com/shiniofthegami/regionaltextures/listeners/PlayerListener.java
@@ -4,7 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
@@ -12,7 +14,6 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 import com.shiniofthegami.regionaltextures.base.Overlay;
 import com.shiniofthegami.regionaltextures.handlers.OverlayHandler;
 import com.shiniofthegami.regionaltextures.handlers.PackHandler;
-import com.shiniofthegami.regionaltextures.util.Debugger;
 
 public class PlayerListener implements Listener{
 	
@@ -27,7 +28,7 @@ public class PlayerListener implements Listener{
 	}
 	
 	@EventHandler
-	public void onPlayerLogin(PlayerLoginEvent e){
+	public void onPlayerJoin(PlayerJoinEvent e){
 		Player p = e.getPlayer();
 		Location loc = p.getLocation();
 		Overlay o = OverlayHandler.getOverlay(loc);
